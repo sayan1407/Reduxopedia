b33cba3690c9124652afd97a6afd7845af119c61
Sayan Saha <sayan199783@gmail.com>
2025-01-06 15:25:44 +0530

    Create first reducer and actions

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './app/Layout/Header';
 import { Provider } from 'react-redux';
-import { store } from './Store/store';
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
